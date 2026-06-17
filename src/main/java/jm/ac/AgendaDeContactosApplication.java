02e14de00db2653b25c9f8ea75d61e276f53fd94
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-17 09:43:28 -0300

    inicializando proyecto en spring boot

---
+++ src/main/java/jm/ac/AgendaDeContactosApplication.java
@@ -0,0 +1,13 @@
+package jm.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeContactosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jm/ac/AgendaDeContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package jm.ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDeContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
