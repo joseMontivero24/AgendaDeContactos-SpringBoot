d2defecdff528ef7b6fc391a132b88a19c56a956
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-17 10:38:38 -0300

    feat: pakage modelo-entity, clase Contacto agregada

---
+++ src/main/java/jm/ac/modelo/Contacto.java
@@ -0,0 +1,25 @@
+package jm.ac.modelo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Contacto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_contacto")
+    private Integer idContacto;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String direccion;
+
+}
