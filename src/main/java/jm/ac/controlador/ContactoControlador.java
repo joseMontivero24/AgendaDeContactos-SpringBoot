2188b2caedbfa35433c755176396d77527e57c50
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-18 10:31:04 -0300

    feat: implementando el metodo obtenerContacto

--- src/main/java/jm/ac/controlador/ContactoControlador.java
+++ src/main/java/jm/ac/controlador/ContactoControlador.java
@@ -1,13 +1,16 @@
 package jm.ac.controlador;
 
-import jm.ac.servicio.ContactoServicio;
+import jm.ac.modelo.Contacto;
 import jm.ac.servicio.IContactoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("ac-app")
@@ -17,4 +20,11 @@ public class ContactoControlador {
 
     @Autowired
     private IContactoServicio contactoServicio;
+
+    @GetMapping("/contactos")
+    public List<Contacto> obtenerContacto(){
+        var contactos = contactoServicio.listarContactos();
+        contactos.forEach((contacto -> logger.info(contacto.toString())));
+        return contactos;
+    }
 }
